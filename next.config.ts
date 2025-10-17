809cfa2ab7e5f8474497cdb0a2baa9a6a2637d40
shafin115 <shafinjunayed954@gmail.com>
2025-10-17 22:33:09 +0600

    feat: disable Turbopack in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // @ts-ignore
+    turbopack: false, // disables Turbopack
+  } as any,
 };
 
 export default nextConfig;
